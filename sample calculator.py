d682c1101dddb6ac5b797d2d8c7c3f63ecb423ad
Abhishek Kumar Singh <119054351+12203279abhi@users.noreply.github.com>
2022-12-08 22:29:15 +0530

    Add files via upload

---
+++ sample calculator.py
@@ -0,0 +1,14 @@
+#sample calculator
+a=int(input())
+b=int(input())
+character=input()
+if character=='+' :
+    print(a+b)
+elif character=="-" :
+    print(a-b)
+elif character=="*" :
+    print(a*b)
+elif character=="/" :
+    print(a/b)
+elif character=="%" :
+    print(a%b)
